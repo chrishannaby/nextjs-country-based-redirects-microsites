31ae74286ba81a26db0c8e32811fc59c4155d8cf
Chris Hannaby <chris.hannaby@netlify.com>
2022-03-23 18:38:29 -0600

    install missing package and move files

--- stores/next.config.js
+++ stores/next.config.js
@@ -1,9 +1,9 @@
-const { LOCALES, DEFAULT_LOCALE, STORE_COUNTRY } = process.env;
+const { LOCALES, DEFAULT_LOCALE, STORE_COUNTRY_PREFIX } = process.env;
 
 const locales = LOCALES.split(",");
 
 module.exports = {
-  basePath: `/${STORE_COUNTRY}`,
+  basePath: STORE_COUNTRY_PREFIX ? `/${STORE_COUNTRY_PREFIX}` : "/",
   i18n: {
     locales,
     defaultLocale: DEFAULT_LOCALE,
